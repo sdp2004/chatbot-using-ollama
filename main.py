197aa8dc395084f120c0daab73ce3203403aac24
sdp092004 <imh10022.22@bitmesra.ac.in>
2024-06-01 12:41:20 +0530

    A chatbot made using Azure OpenAI with Langchain+Sql

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ response = client.chat.completions.create(
     model="chatbotai", # model = "deployment_name".
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello"},
-        {"role": "assistant", "content": "Hi, How can i help you?"},
+        {"role": "user", "content": "What is the name of a Country starts with A? "},
+        {"role": "assistant", "content": "There are so many countries like that. One of them is Afghanistan."},
           ]
 )
 
@@ -41,7 +41,7 @@ engine = create_engine("sqlite:///countrydataset.db")
 db = SQLDatabase(engine=engine)
 print(db.dialect)
 print(db.get_usable_table_names())
-db.run("SELECT * FROM countrydataset WHERE Country_Name LIKE 'A%';")
+db.run("SELECT * FROM countrydataset WHERE 'Country_Name' LIKE 'A%';")
 from langchain_community.agent_toolkits import create_sql_agent
 from langchain_openai import AzureOpenAI
 import os
@@ -49,10 +49,10 @@ from langchain_community.agent_toolkits.sql.base import create_sql_agent
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits.sql.toolkit import SQLDatabaseToolkit
 from langchain_openai import AzureChatOpenAI
-
+from langchain.agents.agent_types import AgentType
 
 
 llm = AzureChatOpenAI(model="chatbotai",api_version="2024-02-01", azure_endpoint = os.getenv("OPENAI_ENDPOINT"),temperature=0)
 toolkit = SQLDatabaseToolkit(db=db, llm=llm)
-agent_executor = create_sql_agent(llm=llm,toolkit=toolkit,agent_type='',verbose=False,handle_parsing_errors=True)
-agent_executor.invoke({"input": "what are country names starting with A"})
+agent_executor = create_sql_agent(llm=llm,toolkit=toolkit,agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,verbose=True,handle_parsing_errors=True)
+agent_executor.invoke({"input": "what are the country names starting with A"})
